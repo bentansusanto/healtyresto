79ada1afa418e6a91f33a761b7b867c594aeb2c4
bentansusanto <tansusanto194@gmail.com>
2022-11-23 22:38:33 +0700

    update about page : add testimoni section

--- src/features/ProductSlice.jsx
+++ src/features/ProductSlice.jsx
@@ -1,10 +1,18 @@
 import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+// get Product
 export const getProducts = createAsyncThunk("products/getProducts", async() => {
     const response = await axios.get(' http://localhost:5000/products')
     return response.data;
 })
+
+// get Category
+export const getCategory = createAsyncThunk('category/getCategory', async(value) => {
+    const response = await axios.get(`http://localhost:5000/product?category=Healty Food`);
+    console.log(response.data);
+})
+
 
 const productEntity = createEntityAdapter({
     selectId : (product) => product.id
@@ -16,7 +24,8 @@ const productSlice = createSlice({
     extraReducers : {
         [getProducts.fulfilled] : (state,action) => {
             productEntity.setAll(state, action.payload);
-        }
+        },
+        
     }
 });
 
